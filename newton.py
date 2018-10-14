05594412e9ee6ef14c5f26467f50e3591dfa01bc
Keith Lindsay <klindsay@ucar.edu>
2018-10-14 13:07:30 -0600

    make private all class members and methods that can be private
    Changes to be committed:
    	modified:   newton.py

--- newton.py
+++ newton.py
@@ -53,32 +53,38 @@ class NewtonState:
     """
     class for representing the state of the Newton's method solver
 
-    members are:
-    workdir             directory where files of values are located
-    state_fname         name of file where solver state is stored
-    iter                current iteration
-    steps_completed     steps of solver that have been completed in the current iteration
+    There are no public members.
+
+    Private members are:
+    _workdir            directory where files of values are located
+    _state_fname        name of file where solver state is stored
+    _iter               current iteration
+    _steps_completed    steps of solver that have been completed in the current iteration
     """
 
     def __init__(self, workdir, state_fname, resume):
         """initialize solver state"""
-        self.workdir = workdir
-        self.state_fname = os.path.join(self.workdir, state_fname)
-        self.steps_completed = []
+        self._workdir = workdir
+        self._state_fname = os.path.join(self._workdir, state_fname)
+        self._steps_completed = []
         if resume:
-            self.read()
+            self._read()
         else:
-            self.iter = 0
+            self._iter = 0
 
     def inc_iter(self):
         """increment iter, reset steps_completed"""
-        self.iter += 1
-        self.steps_completed = []
-        self.write()
+        self._iter += 1
+        self._steps_completed = []
+        self._write()
+
+    def get_iter(self):
+        """return value of iter"""
+        return self._iter
 
     def is_set(self, val_name):
         """has val_name been set in the current iteration"""
-        return val_name+'_set' in self.steps_completed
+        return val_name+'_set' in self._steps_completed
 
     def set_val(self, val_name, val):
         """
@@ -88,10 +94,10 @@ class NewtonState:
         value is written to a value-specific file
         store in steps_completed that the value has been set
         """
-        fname = os.path.join(self.workdir, val_name+'_%02d.nc'%self.iter)
+        fname = os.path.join(self._workdir, val_name+'_%02d.nc'%self._iter)
         file_wrap.write_var(fname, val_name, val)
-        self.steps_completed.append(val_name+'_set')
-        self.write()
+        self._steps_completed.append(val_name+'_set')
+        self._write()
 
     def get_val(self, val_name):
         """
@@ -103,36 +109,36 @@ class NewtonState:
         """
         if not self.is_set(val_name):
             raise Exception(val_name+' not set')
-        fname = os.path.join(self.workdir, val_name+'_%02d.nc'%self.iter)
+        fname = os.path.join(self._workdir, val_name+'_%02d.nc'%self._iter)
         return file_wrap.read_var(fname, val_name)
 
-    def write(self):
+    def log(self):
+        """write solver state to log"""
+        logger = logging.getLogger(__name__)
+        logger.info('iter=%d', self._iter)
+        for step_name in self._steps_completed:
+            logger.info('%s completed', step_name)
+
+    def _write(self):
         """
         write solver state to a file
 
         store the state in a dictionary and write, using JSON
         """
-        obj = {'iter':self.iter, 'steps_completed':self.steps_completed}
-        with open(self.state_fname, mode='w') as fptr:
+        obj = {'iter':self._iter, 'steps_completed':self._steps_completed}
+        with open(self._state_fname, mode='w') as fptr:
             json.dump(obj, fptr, indent=1)
 
-    def read(self):
+    def _read(self):
         """
         read solver state from a file
 
         read, using JSON, dictionary form of state, and extract vals
         """
-        with open(self.state_fname, mode='r') as fptr:
+        with open(self._state_fname, mode='r') as fptr:
             obj = json.load(fptr)
-        self.iter = obj['iter']
-        self.steps_completed = obj['steps_completed']
-
-    def log(self):
-        """write solver state to log"""
-        logger = logging.getLogger(__name__)
-        logger.info('iter=%d', self.iter)
-        for step_name in self.steps_completed:
-            logger.info('%s completed', step_name)
+        self._iter = obj['iter']
+        self._steps_completed = obj['steps_completed']
 
 def _parse_args():
     """parse command line arguments"""
@@ -177,7 +183,7 @@ def main(args):
     solver_state.log()
     comp_fcn(solver_state)
     fcn_val = solver_state.get_val('fcn')
-    logger.info('iter=%d, iterate=%e, y=%e', solver_state.iter, iterate, fcn_val)
+    logger.info('iter=%d, iterate=%e, y=%e', solver_state.get_iter(), iterate, fcn_val)
 
     while np.abs(fcn_val) > 1.0e-10:
         comp_increment(solver_state)
@@ -187,7 +193,7 @@ def main(args):
         solver_state.set_val('iterate', iterate)
         comp_fcn(solver_state)
         fcn_val = solver_state.get_val('fcn')
-        logger.info('iter=%d, iterate=%e, y=%e', solver_state.iter, iterate, fcn_val)
+        logger.info('iter=%d, iterate=%e, y=%e', solver_state.get_iter(), iterate, fcn_val)
 
 if __name__ == '__main__':
     main(_parse_args())
