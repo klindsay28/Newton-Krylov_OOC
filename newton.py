22e6a0409e2f1511c2e911240d2ebf08232d71d1
Keith Lindsay <klindsay@ucar.edu>
2018-10-22 09:55:15 -0600

    rename NewtonState to SolverState and move it to solver.py
    Changes to be committed:
    	modified:   newton.py
    	new file:   solver.py

--- newton.py
+++ newton.py
@@ -3,71 +3,11 @@
 
 import argparse
 import errno
-import json
 import logging
 import os
 import sys
+from solver import SolverState
 from model import ModelState
-
-class NewtonState:
-    """
-    class for representing the state of the Newton's method solver
-
-    There are no public members.
-
-    Private members are:
-    _workdir            directory where files of values are located
-    _state_fname        name of file where solver state is stored
-    _saved_state        dictionary of members saved and recovered across invocations
-        iteration           current iteration
-        step_log            steps of solver that have been logged in the current iteration
-    """
-
-    def __init__(self, workdir, state_fname, resume):
-        """initialize solver state"""
-        self._workdir = workdir
-        self._state_fname = os.path.join(self._workdir, state_fname)
-        if resume:
-            self._read_saved_state()
-        else:
-            self._saved_state = {'iteration':0, 'step_log':[]}
-
-    def inc_iteration(self):
-        """increment iteration, reset step_log"""
-        self._saved_state['iteration'] += 1
-        self._saved_state['step_log'] = []
-        self._write_saved_state()
-        return self._saved_state['iteration']
-
-    def get_iteration(self):
-        """return value of iteration"""
-        return self._saved_state['iteration']
-
-    def step_logged(self, step):
-        """has step been logged in the current iteration"""
-        return step in self._saved_state['step_log']
-
-    def log_step(self, step):
-        """log a step for the current iteration"""
-        self._saved_state['step_log'].append(step)
-        self._write_saved_state()
-
-    def log_saved_state(self):
-        """write saved state of solver to log"""
-        logger = logging.getLogger(__name__)
-        logger.info('iteration=%d', self._saved_state['iteration'])
-        for step_name in self._saved_state['step_log']:
-            logger.info('%s completed', step_name)
-
-    def _write_saved_state(self):
-        """write _saved_state to a JSON file"""
-        with open(self._state_fname, mode='w') as fptr:
-            json.dump(self._saved_state, fptr, indent=2)
-
-    def _read_saved_state(self):
-        """read _saved_state from a JSON file"""
-        with open(self._state_fname, mode='r') as fptr:
-            self._saved_state = json.load(fptr)
 
 class NewtonSolver:
     """class for applying Newton's method to approximate the solution of system of equations"""
@@ -76,7 +16,7 @@ class NewtonSolver:
         """initialize Newton solver"""
 
         self._workdir = workdir
-        self.solver_state = NewtonState(workdir, solver_state_fname, resume)
+        self.solver_state = SolverState(workdir, solver_state_fname, resume)
         self.solver_state.log_saved_state()
 
         # get solver started on an initial run
