bfdae28adf509758d7e1293738fdbd3e6a3163e8
Keith Lindsay <klindsay@ucar.edu>
2020-06-11 11:47:37 -0600

    correct travis_long_dye_decay.sh script
    Changes to be committed:
    	modified:   scripts/travis_long_dye_decay.sh

--- scripts/travis_long_dye_decay.sh
+++ scripts/travis_long_dye_decay.sh
@@ -5,7 +5,8 @@ source scripts/newton_krylov_env_cmds
 # setup and run solver
 echo running setup_solver.sh for dye_decay
 ./scripts/setup_solver.sh --fp_cnt 1 --nlevs 20 --persist \
-    --tracer_module_names iage \
-    --workdir $HOME/travis_long_iage_workdir
+    --tracer_module_names dye_decay_{suff}:001:010 \
+    --newton_rel_tol "1.0e-6" \
+    --workdir $HOME/travis_long_dye_decay_workdir
 echo running nk_driver.sh for dye_decay
-$HOME/travis_long_iage_workdir/nk_driver.sh
+$HOME/travis_long_dye_decay_workdir/nk_driver.sh
