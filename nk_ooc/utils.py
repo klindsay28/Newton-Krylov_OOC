2f45bb63dba477ee063d9eb1991afe9a3f237f00
Keith Lindsay <klindsay@ucar.edu>
2021-04-01 19:43:13 -0600

    avoid anti-pattern identified by deepsource
    Changes to be committed:
    	modified:   nk_ooc/utils.py

--- nk_ooc/utils.py
+++ nk_ooc/utils.py
@@ -128,10 +128,9 @@ def strtobool(val):
     val = val.lower()
     if val in ("y", "yes", "t", "true", "on", "1"):
         return 1
-    elif val in ("n", "no", "f", "false", "off", "0"):
+    if val in ("n", "no", "f", "false", "off", "0"):
         return 0
-    else:
-        raise ValueError("invalid truth value %r" % (val,))
+    raise ValueError("invalid truth value %r" % (val,))
 
 
 ################################################################################
